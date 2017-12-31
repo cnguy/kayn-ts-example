dadc9a54c4e60ed89802ed3da96b9b636a0cfcd3
cnguy <chautnguyen96@gmail.com>
2017-12-31 00:06:48 -0800

    Add not-nullable example

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,21 +5,37 @@ const { Kayn, REGIONS } = lolapi
 
 const kayn = Kayn()()
 
+import { MatchV3MatchReferenceDto } from 'kayn/typings/dtos';
+
+const NotFoundRes = (res: any) => res.status(404).send({ code: 404, message: 'Not Found' })
+
 export default express.Router()
   .get('/', (req, res) => {
-    res.status(200).send({ hello: 'test' });
+    res.status(200).send({ hello: 'world' });
   })
+  /* localhost:8080/api/summoners/:name/matchlist/game-ids-of-past-five-games */
   .get('/api/summoners/:name', async (req, res) => {
     try {
-      // const summoner = await kayn.Summoner.by.name(req.params.name)
       const summoner = await kayn.Summoner.by.name(req.params.name).region(REGIONS.NORTH_AMERICA)
-      const name : string | undefined = summoner.name // This can be improved hmm..
       res.json(summoner)
     } catch (ex) {
-      res.status(404).send({
-        code: 404,
-        message: 'Not Found',
+      NotFoundRes(res)
+    }
+  })
+  /* localhost:8080/api/summoners/:name/matchlist/game-ids-of-past-five-games */
+  .get('/api/summoners/:name/matchlist/game-ids-of-past-five-games', async (req, res) => {
+    try {
+      const summoner = await kayn.Summoner.by.name(req.params.name)
+      const accountID = summoner.accountId as number
+      const matchlist = await kayn.Matchlist.by.accountID(accountID).query({
+        beginIndex: 0,
+        endIndex: 5,
       })
+      const matches = matchlist.matches as MatchV3MatchReferenceDto[]
+      const gameIDs = matches.map(match => match.gameId)
+      res.json(gameIDs)
+    } catch (ex) {
+      NotFoundRes(res)
     }
   })
 
