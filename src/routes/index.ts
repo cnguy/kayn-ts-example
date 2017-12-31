16bd7cd7702b378d684b484e26fb22e3e5211224
cnguy <chautnguyen96@gmail.com>
2017-12-31 00:26:39 -0800

    Cleanup with commentssss

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,23 @@
 /// <reference path="../../node_modules/@types/express/index.d.ts" />
-import * as express from 'express';
+import * as express from 'express'
 import * as lolapi from 'kayn'
 const { Kayn, REGIONS } = lolapi
 
+// Type-safe config instead of just passing in a regular object.
+const kaynConfig: KaynConfig = {
+  region: REGIONS.NORTH_AMERICA,
+  requestOptions: {
+    shouldRetry: true,
+    numberOfRetriesBeforeAbort: 3,
+    delayBeforeRetry: 3000,
+  },
+  // hello: 'world', // fails
+}
+
 const kayn = Kayn()()
 
-import { MatchV3MatchReferenceDto } from 'kayn/typings/dtos';
+import { MatchV3MatchReferenceDto, SummonerV3SummonerDTO } from 'kayn/typings/dtos'
+import { KaynConfig } from 'kayn';
 
 const NotFoundRes = (res: any) => res.status(404).send({ code: 404, message: 'Not Found' })
 
@@ -13,10 +25,10 @@ export default express.Router()
   .get('/', (req, res) => {
     res.status(200).send({ hello: 'world' });
   })
-  /* localhost:8080/api/summoners/:name/matchlist/game-ids-of-past-five-games */
+  /* localhost:8080/api/summoners/:name */
   .get('/api/summoners/:name', async (req, res) => {
     try {
-      const summoner = await kayn.Summoner.by.name(req.params.name).region(REGIONS.NORTH_AMERICA)
+      const summoner = await kayn.Summoner.by.name(req.params.name).region(REGIONS.NORTH_AMERICA) // Type inference.
       res.json(summoner)
     } catch (ex) {
       NotFoundRes(res)
@@ -25,14 +37,14 @@ export default express.Router()
   /* localhost:8080/api/summoners/:name/matchlist/game-ids-of-past-five-games */
   .get('/api/summoners/:name/matchlist/game-ids-of-past-five-games', async (req, res) => {
     try {
-      const summoner = await kayn.Summoner.by.name(req.params.name)
-      const accountID = summoner.accountId as number
-      const matchlist = await kayn.Matchlist.by.accountID(accountID).query({
+      const summoner: SummonerV3SummonerDTO = await kayn.Summoner.by.name(req.params.name) // Explicit type. Auto-imported by VSCode.
+      const accountID: number = summoner.accountId! // Not-nullable assertion.
+      const matchlist = await kayn.Matchlist.by.accountID(accountID).query({ // Type inference.
         beginIndex: 0,
         endIndex: 5,
       })
-      const matches = matchlist.matches as MatchV3MatchReferenceDto[]
-      const gameIDs = matches.map(match => match.gameId)
+      const matches = matchlist.matches as MatchV3MatchReferenceDto[] // Type-casting.
+      const gameIDs: Array<number> = matches.map(match => match.gameId!) // Not-nullable assertion.
       res.json(gameIDs)
     } catch (ex) {
       NotFoundRes(res)
